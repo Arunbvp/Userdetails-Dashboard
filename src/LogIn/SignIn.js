feed0c9c8d133f4c1552632af11f3db9e864d40d
Arunbvp <arunkumar.bala95@gmail.com>
2023-06-28 20:14:53 +0530

    Logo changes

---
+++ src/LogIn/SignIn.js
@@ -0,0 +1,33 @@
+import React from "react";
+import microsoftLogo from "../assets/login/microsoft.png";
+import googleLogo from "../assets/login/google.png";
+import "./SignIn.css";
+
+const SignIn = () => {
+  const handleMicrosoftLogin = () => {
+    console.log("Microsoft login clicked");
+  };
+
+  const handleGoogleLogin = () => {
+    console.log("Google login clicked");
+  };
+
+  return (
+    <div className="sign-in-container">
+      <h2 className="sign-in-title">Sign In</h2>
+      <div className="sign-in-options">
+        <button
+          className="sign-in-button microsoft"
+          onClick={handleMicrosoftLogin}
+        >
+          <img src={microsoftLogo} alt="Microsoft" className="sign-in-logo" />
+        </button>
+        <button className="sign-in-button google" onClick={handleGoogleLogin}>
+          <img src={googleLogo} alt="Google" className="sign-in-logo" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
