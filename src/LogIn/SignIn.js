f10e864abe52b515dec1369b73f9b53aa0ffae75
Arunbvp <arunkumar.bala95@gmail.com>
2023-06-28 23:18:14 +0530

    Design changes

--- src/LogIn/SignIn.js
+++ src/LogIn/SignIn.js
@@ -14,17 +14,18 @@ const SignIn = () => {
 
   return (
     <div className="sign-in-container">
-      <h2 className="sign-in-title">Sign In</h2>
+      {/* <h2 className="sign-in-title">Sign In</h2> */}
       <div className="sign-in-options">
         <button
           className="sign-in-button microsoft"
           onClick={handleMicrosoftLogin}
         >
           <img src={microsoftLogo} alt="Microsoft" className="sign-in-logo" />
+          <span>Continue with Microsoft</span>
         </button>
-        <button className="sign-in-button google" onClick={handleGoogleLogin}>
+        {/* <button className="sign-in-button google" onClick={handleGoogleLogin}>
           <img src={googleLogo} alt="Google" className="sign-in-logo" />
-        </button>
+        </button> */}
       </div>
     </div>
   );
